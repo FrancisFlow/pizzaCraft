f47a591d32c086e92d6a3e72dedcb0f1af3f58cb
FrancisFlow <iamngigikaranja1@aol.com>
2021-08-03 14:31:14 +0300

    Created  deliverMe function for delivery option

--- js/index.js
+++ js/index.js
@@ -24,27 +24,46 @@
 
 // var x = document.getElementById("#size").value
 
-function totalPrice (size, crust, toppings){
+function totalPrice(size, crust, toppings) {
   var size = document.querySelector("#size").value;
-  var crust= document.querySelector("#crust").value;
+  var crust = document.querySelector("#crust").value;
   var toppings = document.querySelector("#toppings").value;
-
-  size= parseInt(size);
- crust= parseInt(crust);
- toppings= parseInt(toppings);
- return (size+crust+toppings)*quantity;
+  var quantity = document.querySelector("#quantity").value;
+  size = parseInt(size);
+  crust = parseInt(crust);
+  toppings = parseInt(toppings);
+  quantity = parseInt(quantity);
+  return (size + crust + toppings) * quantity;
 }
+
+// display price to users
+
 function showSizePrice(size) {
   var size = document.querySelector("#size").value;
 
   return document.querySelector("#pizzaSize").innerHTML = "Price is " + size;
 }
-function showCrustPrice (crust) {
-    var crust= document.querySelector("#crust").value;
-    return document.querySelector("#crust-Selected").innerHTML = "Price is " + crust;
+
+function showCrustPrice(crust) {
+  var crust = document.querySelector("#crust").value;
+  return document.querySelector("#crust-Selected").innerHTML = "Price is " + crust;
 
 }
+
 function showToppingsPrice(toppings) {
   var toppings = document.querySelector("#toppings").value;
   return document.querySelector("#toppings-selected").innerHTML = "Price is " + toppings;
+}
+
+function showTotalPrice() {
+  return document.querySelector("#checkList").innerHTML = "Total price is " + totalPrice();
+}
+// Delivery function
+function deliverMe() {
+  if (document.querySelector("#deliveryOption").value == "Yes") {
+    var askLocation = prompt("Please enter delivery location");
+    return askLocation;
+  } else if (document.querySelector("#deliveryOption").value=="No"){
+    return console.log(document.querySelector("#deliveryOption").value);
+  }
 }
