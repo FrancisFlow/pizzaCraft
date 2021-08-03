7053d14e2d2cbf51bfa5d43af1cdb8ca9340bb0e
FrancisFlow <iamngigikaranja1@aol.com>
2021-08-03 09:23:01 +0300

    Created a constractor function

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
-if (delivery=true;){
-location= prompt("What is your location");
+
+
+
+class Pizza {
+  constractor(size, topping, crust) {
+    var size = $("#size").value;
+    var toppings = $("#toppings").value;
+    var crust = $("#crust").value;
+    this.size = size;
+    this.topping = toppings;
+    this.crust = crust;
+  }
+}
+
+
+
+var pizzaPrice=[1000, 800, 600, 400, 200];
+var crustPrice =[400, 300, 200, 100, 50];
+var toppings = [300, 200, 300, 400, 500];
+
+function totalPrice (size, toppings, crust) {
+  
 }
